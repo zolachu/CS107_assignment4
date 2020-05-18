4a14052ea13547f81f86f17602f73e21fc449346
CS107 tools <cs107@cs.stanford.edu>
2020-05-18 16:46:30 -0700

    This project passes 5 of the 9 default sanity check cases.

--- util.c
+++ util.c
@@ -1,7 +1,31 @@
 #include "samples/prototypes.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 void *binsert(const void *key, void *base, size_t *p_nelem, size_t width,
               int (*compar)(const void *, const void *)) {
     // TODO: implement this function (and remove the line below)
-    return NULL;
+
+  char *base1 = (char*)base;
+
+  for(size_t nremain = *p_nelem; nremain != 0; nremain >>=1) {
+    void *p = base1 + (nremain >> 1)* width;
+    int sign = compar(key, p);
+
+    if (sign == 0) return p; // if matching element is found
+    if (sign > 0) {
+      base1 = (char *)p + width;
+      nremain--;
+    }
+  }
+
+  ++(*p_nelem);
+
+  size_t index = ((char*)base1 - (char*)base)/width;
+
+  memmove((char*)base + (index + 1)* width,
+	  (char*)base + index* width, (*p_nelem - index + 1)* width);
+
+  return  memcpy((char *)base + index * width, key, width);
 }
