0251864d5cbfcafab53b3d80aa3279f3846d436d
CS107 tools <cs107@cs.stanford.edu>
2020-05-18 14:06:48 -0700

    This project passes 3 of the 9 default sanity check cases.

--- mysort.c
+++ mysort.c
@@ -4,6 +4,10 @@
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <search.h>
+#include <assert.h>
 
 #define MAX_LINE_LEN 4096
 #define MIN_NLINES 100
@@ -11,22 +15,81 @@
 typedef int (*cmp_fn_t)(const void *p, const void *q);
 
 int cmp_pstr(const void *p, const void *q) {
-    // TODO: implement this function (and remove the line below)
-    return 0;
+  // TODO: implement this function (and remove the line below)
+  const char *pstr = *(const char **) p;
+  const char *qstr = *(const char **) q;
+
+  return strcmp(pstr, qstr);
 }
 
 int cmp_pstr_len(const void *p, const void *q) {
-    // TODO: implement this function (and remove the line below)
-    return 0;
+  // TODO: implement this function (and remove the line below)
+  const char *pstr = *(const char **) q;
+  const char *qstr = *(const char **) q;
+  int len_p = strlen(pstr);
+  int len_q = strlen(qstr);
+
+  return len_p == len_q ? cmp_pstr(pstr, qstr) : ((len_p > len_q) - (len_p < len_q));
 }
 
 int cmp_pstr_numeric(const void *p, const void *q) {
-    // TODO: implement this function (and remove the line below)
-    return 0;
+  // TODO: implement this function (and remove the line below)
+  const char *s1 = *(const char **) p;
+  const char *s2 = *(const char **) q;
+  int num1 = atoi(s1);
+  int num2 = atoi(s2);
+  return (num1 > num2) - (num1 < num2);
+}
+
+/*
+ *
+ *
+ */
+
+char **getLines(FILE *fp, size_t *n_line, cmp_fn_t cmp, bool uniq) {
+  int size = MIN_NLINES;
+  char **lines = malloc(size * sizeof(char *));
+  assert(lines);
+  char line[MAX_LINE_LEN];
+  while (fgets(line, MAX_LINE_LEN, fp) != NULL) {
+    if (*n_line == size) {
+      size *= 2;
+      lines = realloc(lines, size * sizeof(char *));
+    }
+
+    if (!uniq) {
+      lines[*n_line] = strdup(line);
+      (*n_line)++;
+    } else {
+      char *key = line;
+      char **new = binsert(&key, lines, n_line, sizeof(char *), cmp);
+      if (key == *new) {
+	*new = strdup(key);
+      }
+    }
+  }
+  return lines;
+
 }
 
 void sort_lines(FILE *fp, cmp_fn_t cmp, bool uniq, bool reverse) {
-    // TODO: implement this function
+  // TODO: implement this function
+  size_t n_line = 0;
+  char **lines = getLines(fp, &n_line, cmp, uniq);
+  qsort(lines, n_line, sizeof(char *), cmp);
+  if (reverse) {
+    while (n_line--) {
+      printf("%s", lines[n_line]);
+      free(lines[n_line]);
+    }
+  } else {
+    for (int i = 0; i < n_line; i++) {
+      printf("%s", lines[i]);
+      free(lines[i]);
+    }
+
+  }
+  free(lines);
 }
 
 int main(int argc, char *argv[]) {
