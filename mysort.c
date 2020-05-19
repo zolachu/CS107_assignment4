05d12b295aa5fa1c6828bad7338f6859bd068191
CS107 tools <cs107@cs.stanford.edu>
2020-05-19 00:41:36 -0700

    This project passes 8 of the 9 default sanity check cases.

--- mysort.c
+++ mysort.c
@@ -68,7 +68,7 @@ char **getUniqueLines(FILE *fp, size_t *n_line, cmp_fn_t cmp) {
       size *= 2;
       printf("%d", size);
       lines = (char**)realloc(lines, size * sizeof(char *));
-      assert(lines);
+      //      assert(lines);
     }
 
     char *key = line;
