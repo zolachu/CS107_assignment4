3608e23d47439199f3290322d44c841d29282560
CS107 tools <cs107@cs.stanford.edu>
2020-05-18 21:31:18 -0700

    This project passes 3 of the 5 custom test cases read from file custom_tests.

--- mysort.c
+++ mysort.c
@@ -92,8 +92,8 @@ void sort_lines(FILE *fp, cmp_fn_t cmp, bool uniq, bool reverse) {
   size_t n_line = 0;
   char **lines = getLines(fp, &n_line, cmp_pstr, uniq);
   assert(lines);
-  //  qsort(lines, n_line, sizeof(char *), cmp_pstr);
-    qsort(lines, n_line, sizeof(char *), cmp);
+    qsort(lines, n_line, sizeof(char *), cmp_pstr);
+    //  qsort(lines, n_line, sizeof(char *), cmp);
   if (reverse) {
     /* printt the lines in the reverse order */
     while (n_line--) {
