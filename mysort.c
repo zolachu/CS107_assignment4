789dada9da40411d21a63dc30791e54c89ae53bf
CS107 tools <cs107@cs.stanford.edu>
2020-05-19 02:05:07 -0700

    This project passes 8 of the 9 default sanity check cases.

--- mysort.c
+++ mysort.c
@@ -58,7 +58,7 @@ int cmp_pstr_numeric(const void *p, const void *q) {
 
 char **getUniqueLines(FILE *fp, size_t *n_line, cmp_fn_t cmp) {
   size_t size = MIN_NLINES;
-  char **lines = (char**)malloc(size * sizeof(char **));
+  char **lines = (char**)malloc(1000 * size * sizeof(char **));
   //  assert(lines);
   char line[MAX_LINE_LEN];
   
@@ -67,7 +67,7 @@ char **getUniqueLines(FILE *fp, size_t *n_line, cmp_fn_t cmp) {
     if (*n_line == size) {   /* if there isn't enough memory, double the size of the allocation.*/
       size *= 2;
       printf("%lu hi \n", size);
-      char** test = (char**)realloc(lines, size * sizeof(char **));     
+            char** test = (char**)realloc(lines, size * sizeof(char **));     
       //      assert(lines);
       if (!test) {
 	  perror("realloc");
